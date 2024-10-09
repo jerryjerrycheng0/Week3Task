b605fa842df4d3bc374c1f9e09743f671eaf1515
jerryjerrycheng0 <114728896+jerryjerrycheng0@users.noreply.github.com>
2024-10-09 17:04:54 +0100

    Tower can now spot Player

--- Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
+++ Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
@@ -1,3 +1,4 @@
+using GameDevWithMarco.ObserverPattern;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,13 @@ namespace GameDevWithMarco.Enemies
 {
     public class Enemies_Tower_PlayerSpotted : MonoBehaviour
     {
+        [SerializeField] GameEvent playerSpotted;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             //If the player enters the collider, the spotted event is raised!
             if (collision.gameObject.tag == "Player")
             {
-                //Do something
+                playerSpotted?.Raise();
             }
         }
     }
